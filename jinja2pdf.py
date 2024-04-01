a454d6b496e643178cbbf76249fd2e9338872bc7
akhilbabu <akhilbabu.mpn@gmail.com>
2024-04-01 10:22:23 +0530

    Added imports for creating directory is doesn't exist

--- jinja2pdf.py
+++ jinja2pdf.py
@@ -3,14 +3,13 @@ import json
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
 
-directory_name='renders'
-template_name = 't2table.html'
-data_name = 'data1.json'
+directory_name = "renders"
+template_name = "t2table.html"
+data_name = "data1.json"
 
 if not os.path.exists(directory_name):
     os.makedirs(directory_name)
 
-import os
 
 def generate_pdf(html_content, output_filename):
     try:
@@ -21,18 +20,23 @@ def generate_pdf(html_content, output_filename):
         print(f"Error generating {output_name}: {e}")
         return False
 
-with open(data_name, 'r') as file:
+
+with open(data_name, "r") as file:
     entries = json.load(file)
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader("."))
 template = env.get_template(template_name)
 
 for index, entry in enumerate(entries):
     html_out = template.render(entry)
-    
+
     pdf_filename = f'{template_name}_{index+1}_{entry.get("form_id", "unknown")}.pdf'
-    
+
     if generate_pdf(html_out, pdf_filename):
-        print(f'{index + 1}. Successfully generated PDF for {entry.get("form_id", "unknown")}: {pdf_filename}')
+        print(
+            f'{index + 1}. Successfully generated PDF for {entry.get("form_id", "unknown")}: {pdf_filename}'
+        )
     else:
-        print(f'{index + 1}. Failed to generate PDF for {entry.get("form_id", "unknown")}')
+        print(
+            f'{index + 1}. Failed to generate PDF for {entry.get("form_id", "unknown")}'
+        )
